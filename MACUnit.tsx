adbdc21ce6f0d25082ed157f795c2aa6d610a6c8
william pan <williampan4032@gmail.com>
2025-03-28 11:48:23 -0700

    Systolic Array v7 coloring

--- MACUnit.tsx
+++ MACUnit.tsx
@@ -49,14 +49,14 @@ export const MACUnit = ({ className, left_input, left_input_flush, top_input, we
       <div className="weight_r absolute flex justify-center items-center absolute w-[50px] h-[25px] bg-black rounded top-[20%] left-1/3 -translate-x-1/2 -translate-y-1/2 border border-neutral-700">
         <p className={`absolute font-mono text-white text-xs text-center select-none ${(() => {
           switch (true) {
-            case ((animationPhase === 'update' || animationPhase === 'translate') && !is_new_weight && weight_active):
-              return 'text-green-300';
-            case (animationPhase === 'update' && weight_active && is_new_weight):
+            case ((animationPhase === 'update' || animationPhase === 'translate') && !is_new_weight && acc_active):
+              return 'text-rose-300';
+            case (animationPhase === 'update' && acc_active && is_new_weight):
               return `-top-[${weight_y_offset}px]`;
-            case (animationPhase === 'translate' && weight_active && is_new_weight):
-              return 'top-[3px] visible translate-z-10 transition-all duration-1000 ease-in text-green-300';
-            case (animationPhase === 'idle' && weight_active):
-              return 'text-green-300';
+            case (animationPhase === 'translate' && acc_active && is_new_weight):
+              return 'top-[3px] visible translate-z-10 transition-all duration-1000 ease-in text-rose-300';
+            case (animationPhase === 'idle' && acc_active):
+              return 'text-rose-300';
             default:
               return 'invisible';
           }
@@ -65,14 +65,14 @@ export const MACUnit = ({ className, left_input, left_input_flush, top_input, we
         </p> 
         <p className={`absolute -top-[20px] font-mono text-xs text-center select-none ${(() => {
           switch (true) {
-            case (animationPhase === 'update' && weight_active && is_new_weight):
+            case (animationPhase === 'update' && acc_active && is_new_weight):
               return "text-neutral-500";
-            case (animationPhase === 'update' && weight_active && !is_new_weight):
-              return "text-green-500";
-            case (animationPhase === 'idle' && weight_active):
-              return "text-green-500";
-            case (animationPhase === 'translate' && weight_active):
-              return "text-green-500";
+            case (animationPhase === 'update' && acc_active && !is_new_weight):
+              return "text-rose-700";
+            case (animationPhase === 'idle' && acc_active):
+              return "text-rose-700";
+            case (animationPhase === 'translate' && acc_active):
+              return "text-rose-700";
             default:
               return 'text-neutral-500';
           }
@@ -81,24 +81,65 @@ export const MACUnit = ({ className, left_input, left_input_flush, top_input, we
         </p> 
       </div>  
       <div className="top_input_r absolute flex justify-center items-center absolute w-[50px] h-[25px] bg-black rounded top-[20%] left-2/3 -translate-x-1/2 -translate-y-1/2 border border-neutral-700">
-        <p className="font-mono text-white text-xs text-center select-none">{top_input?.toFixed(2)}</p> 
+        <p className={`font-mono text-xs text-center select-none ${(() => {
+          switch (true) {
+            case (animationPhase === 'update' && top_input_active):
+              return 'text-blue-300 -translate-y-[154px] -translate-x-[50px]';
+            case (animationPhase === 'update' && acc_active):
+              return 'text-black';
+            case (animationPhase === 'translate' && acc_active):
+              return 'text-white transition-all duration-1000 ease-in';
+            case (animationPhase === 'idle' && acc_active):
+              return 'text-white';
+            default:
+              return 'invisible';
+          }
+        })()}`}>{top_input?.toFixed(2)}</p> 
         <p className="absolute -top-[20px] font-mono text-neutral-500 text-xs text-center select-none">top_input_r</p> 
       </div>  
-      <div className="add_r flex justify-center items-center absolute w-[25px] h-[25px] bg-black rounded-full top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none border border-neutral-700">  
+
+      {/* Multiply */}
+      <div className={`add_r flex justify-center items-center absolute w-[25px] h-[25px] bg-black rounded-full top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none border ${animationPhase == 'idle' && acc_active ? 'border-transparent' : 'border-neutral-700'}`} style={{ 
+        backgroundClip: 'padding-box', 
+        boxShadow: '0 0 0 1px transparent, inset 0 0 0 1px rgba(0,0,0,0)', 
+        background: (() => {
+          switch (true) {
+            case (animationPhase === 'idle'):
+              return 'linear-gradient(black, black) padding-box, linear-gradient(to bottom right, #ff0000, #7d40f3) border-box';
+            default:
+              return 'black';
+          }
+        })()
+      }}>  
         <X className="w-4 h-4 text-white" />
       </div>
-      <div className="mul_r flex justify-center items-center absolute w-[25px] h-[25px] bg-black rounded-full top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none border border-neutral-700">
+
+      {/* Plus */}
+      <div className={`mul_r flex justify-center items-center absolute w-[25px] h-[25px] bg-black rounded-full top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none border ${animationPhase == 'idle' && acc_active ? 'border-transparent' : 'border-neutral-700'}`} style={{ 
+        backgroundClip: 'padding-box', 
+        boxShadow: '0 0 0 1px transparent, inset 0 0 0 1px rgba(0,0,0,0)', 
+        background: (() => {
+          switch (true) {
+            case (animationPhase === 'idle'):
+              return 'linear-gradient(black, black) padding-box, linear-gradient(to bottom right, #7d40f3, #3b82f6) border-box';
+            default:
+              return 'black';
+          }
+        })()
+      }}>
         <Plus className="w-4 h-4 text-white" /> 
       </div>
+
+
       <div className="output_r absolute flex justify-center items-center absolute w-[50px] h-[25px] bg-black rounded top-[80%] left-1/2 -translate-x-1/2 -translate-y-1/2 border border-neutral-700">
         <p className={`font-mono text-xs text-center select-none ${(() => {
           switch (true) {
             case (animationPhase === 'update'):
               return 'invisible';
             case (animationPhase === 'translate'):
-              return `invisible`;
+              return `opacity-0`;
             case (animationPhase === 'idle' && acc_active):
-              return 'text-blue-300';
+              return 'text-blue-300 opacity-100 transition-opacity duration-1000 ease-in';
             default:
               return 'invisible';
           }
@@ -106,11 +147,11 @@ export const MACUnit = ({ className, left_input, left_input_flush, top_input, we
         <p className={`absolute top-[25px] font-mono text-xs text-center select-none ${(() => {
           switch (true) {
             case (animationPhase === 'update'):
-              return 'text-neutral-500';
+              return 'text-neutral-500 transition-color duration-1000 ease-in';
             case (animationPhase === 'translate'):
               return `text-neutral-500`;
             case (animationPhase === 'idle' && acc_active):
-              return 'text-blue-500';
+              return 'text-blue-500 transition-color duration-1000 ease-in';
             default:
               return 'text-neutral-500';
           }
@@ -161,25 +202,163 @@ export const MACUnit = ({ className, left_input, left_input_flush, top_input, we
       </div>
 
       {/* Arrows */}
+      {/* Arrow from top to Weight */}
       {is_top ? 
-        <Arrow length={37} chevronSize={10} direction="down" color="#888" className="absolute -top-[15px] left-1/3" /> : 
-        <Arrow length={55} chevronSize={10} direction="down" color="#888" className="absolute -top-[34px] left-1/3" />
+        <Arrow length={37} chevronSize={10} direction="down" color={(() => {
+          switch (animationPhase) {
+            case 'translate':
+              return is_new_weight ? '#d87561' : '#888';
+            default:
+              return '#888';
+          }
+        })()} className="absolute -top-[15px] left-1/3" /> : 
+        <Arrow length={55} chevronSize={10} direction="down" color={(() => {
+          switch (animationPhase) {
+            case 'translate':
+              return is_new_weight ? '#d87561' : '#888';
+            default:
+              return '#888';
+          }
+        })()} className="absolute -top-[34px] left-1/3" />
       }    
-      <Arrow length={35} chevronSize={10} direction="right" color="#888" className="absolute top-1/2 -left-[15px]" />
+
+      {/* Arrow from Input to Input */}
+      {is_left ? 
+      <Arrow 
+        length={35} 
+        chevronSize={10} 
+        direction="right" 
+        color={(() => {
+          switch (animationPhase) {
+            case 'translate':
+              return left_input_active ? '#ffc9c9' : '#888';
+            default:
+              return '#888';
+          }
+        })()} 
+        className="absolute top-1/2 -left-[15px]" 
+      /> :
+      <Arrow 
+        length={265} 
+        chevronSize={10} 
+        direction="right" 
+        color={(() => {
+          switch (animationPhase) {
+            case 'translate':
+              return left_input_active ? '#ffc9c9' : '#888';
+            default:
+              return '#888';
+          }
+        })()} 
+        className="absolute top-1/2 -left-[245px] -z-1" 
+      /> 
+      }
       {is_right ? 
         <Arrow length={235} chevronSize={10} direction="right" color="#888" className="absolute top-1/2 left-[83px]" /> : 
         <Arrow length={235} chevronSize={0} direction="right" color="#888" className="absolute top-1/2 left-[83px]" />
       }
-      <Arrow length={20} chevronSize={10} direction="down" color="#fff" className="absolute top-[46.5%] left-[50%]" />
-      <Arrow length={20} chevronSize={10} direction="down" color="#888" className="absolute top-[66.5%] left-[50%]" />
+
+      {/* Multiply to Plus */}
+      <Arrow 
+        length={20} 
+        chevronSize={10} 
+        direction="down" 
+        color={(() => {
+          switch (animationPhase) {
+            case 'idle':
+              return left_input_active ? '#8c71e4' : '#aaa';
+            default:
+              return '#aaa';
+          }
+        })()} 
+        className="absolute top-[46.5%] left-[50%] z-50" 
+      />
+
+      {/* Plus to Output */}
+      <Arrow 
+        length={20} 
+        chevronSize={10} 
+        direction="down" 
+        color={(() => {
+          switch (animationPhase) {
+            case 'idle':
+              return acc_active ? 'rgb(59, 130, 246)' : '#888';
+            default:
+              return '#888';
+          }
+        })()} 
+        className="absolute top-[66.5%] left-[50%]" 
+      />
       {is_bottom ? 
         <Arrow length={45} chevronSize={10} direction="down" color="#888" className="absolute top-[91%] left-[50%]" /> : 
         <Arrow length={55} chevronSize={0} direction="down" color="#888" className="absolute top-[91%] left-[50%]" />
       }
-      <RoundedArrow points={[[0, 0], [0, 32], [30, 32]]} color="#888" className="absolute top-[27%] left-1/3" radius={5} />
-      <RoundedArrow points={[[0, 0], [0, 17], [20, 17], [20, 46]]} color="#888" className="absolute top-[37.25%] left-[95px] -rotate-90" radius={5} />
-      <RoundedArrow points={[[0, 0], [0, 99], [-30, 99]]} color="#fff" className="absolute top-[27%] left-[57%]" radius={5} />
-      <RoundedArrow points={[[0, 0], [0, 22], [50, 22], [50, 38]]} color="#888" className="absolute -top-[15px] left-1/2" radius={5} />
+      
+      {/* Weight to multiply */}
+      <RoundedArrow 
+        points={[[0, 0], [0, 32], [30, 32]]} 
+        color={(() => {
+          switch (animationPhase) {
+            case 'idle':
+              return left_input_active ? 'rgb(190, 18, 60)' : '#888';
+            default:
+              return '#888';
+          }
+        })()} 
+        className="absolute top-[27%] left-1/3" 
+        radius={5} 
+      />
+
+      {/* Input to multiply */}
+      <RoundedArrow 
+        points={[[0, 0], [0, 17], [20, 17], [20, 46]]} 
+        color={(() => {
+          switch (animationPhase) {
+            case 'idle':
+              return left_input_active ? 'rgb(239, 68, 68)' : '#888';
+            default:
+              return '#888';
+          }
+        })()} 
+        className="absolute top-[37.25%] left-[95px] -rotate-90 z-50" 
+        radius={5} 
+      />
+      <RoundedArrow 
+        points={[[0, 0], [0, 99], [-30, 99]]} 
+        color={(() => {
+          switch (animationPhase) {
+            case 'idle':
+              return left_input_active ? '#e14f58' : '#aaa';
+            default:
+              return '#aaa';
+          }
+        })()} 
+        className="absolute top-[27%] left-[57%] z-50" 
+        radius={5} 
+      />
+      
+      {/* Arrow from output to top input */}
+      {!is_top ? 
+      <RoundedArrow 
+        points={[[0, 0], [0, 67], [50, 67], [50, 83]]} 
+        color={(() => {
+          switch (animationPhase) {
+            case 'translate':
+              return left_input_active && !is_top ? '#90ace0' : '#888';
+            default:
+              return '#888';
+          }
+        })()} 
+        className="absolute -top-[60px] left-1/2" 
+        radius={5} 
+      /> :
+      <RoundedArrow 
+        points={[[0, 0], [0, 22], [50, 22], [50, 38]]} 
+        color='#888'
+        className="absolute -top-[15px] left-1/2" 
+        radius={5} 
+      />
+      }
     </div>
   );
 };
